657e927cac0a1e7b54e69f373273901b0e2ff7bb
wouter.overbeek <wouter.overbeek@leaseplan.com>
2021-07-20 10:02:04 +0200

    adds running p1 client

--- src/run_energy_measurement.py
+++ src/run_energy_measurement.py
@@ -4,7 +4,7 @@ from p1_client import P1client
 from measurement_storage import MeasurementStorer
 
 if __name__ == '__main__':
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%m:%s")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     p = P1client()
     measurement_df = p.read_p1_connection()
     m = MeasurementStorer()
---
+++ src/run_energy_measurement.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/pi/python-projects/energy_consumption/src
+source env/bin/activate
+python3 run_energy_measurement.py
