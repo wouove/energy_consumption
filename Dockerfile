f38a3fbb4484db65e176779482dd5d948d6989e3
wouter.overbeek <wouter.overbeek@leaseplan.com>
2022-01-14 10:19:38 +0100

    commit for clean up

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.6.12-alpine3.12
 
 COPY . .
+COPY test_20211210.txt /storage/test_20211210.txt
 
 #RUN pip install -r requirements.txt
 
-RUN crontab crontab
-
+#RUN crontab crontab
+VOLUME /storage
 CMD ["python", "src/run_energy_measurement.py"]
--- src/run_energy_measurement.py
+++ src/run_energy_measurement.py
@@ -1,11 +1,22 @@
 from datetime import datetime
+import logging
+import time
 
-from p1_client import P1client
-from measurement_storage import MeasurementStorer
+# from p1_client import P1client
+# from measurement_storage import MeasurementStorer
 
 if __name__ == '__main__':
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    p = P1client()
-    measurement_df = p.read_p1_connection()
-    m = MeasurementStorer()
-    m.save_measurement(measurement_df, timestamp)
+    while True:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # p = P1client()
+        # measurement_df = p.read_p1_connection()
+        # m = MeasurementStorer()
+        # m.save_measurement(measurement_df, timestamp)
+
+        # import pandas as pd
+        path = '/storage/test_20211210.txt'
+
+        with open(path, 'w') as f:
+            logging.info(f"Writes to log at {timestamp}")
+            f.write(f"Test at {timestamp} \n")
+        time.sleep(60)
